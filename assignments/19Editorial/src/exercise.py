02859dc11888d2221582e447de550df724e0ff9c
Rodrigo González <a01412633@itesm.mx>
2021-08-26 18:54:39 -0500

    Se realizó el ejercicio de "19Editorial"

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,6 +1,14 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    no_palabras = int(input("Dame el número de palabras: "))
+
+    if no_palabras < 475:
+        print("Debe de haber un mínimo de 475 palabras")
+    else:
+        no_paginas = no_palabras/475
+        costo = (no_paginas * 60)
+        costo_total = costo - ((costo * 10) / 100)
+        print("El costo de la publicación es:", costo_total)
 
 
 if __name__ == '__main__':
